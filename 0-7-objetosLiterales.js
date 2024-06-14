b3c3090b7d938fb41032f06140774e424df8af07
Andres Abdala Alvarez <andrespampa@gmail.com>
2024-06-14 00:18:22 -0300

    terminando Objetos literales

---
+++ 0-7-objetosLiterales.js
@@ -0,0 +1,41 @@
+// ---------------------------------------------| Objetos Literales |--------------------------------------------- //
+
+let nombre = "kEnAi",
+    edad = 7;
+
+const perro ={
+    nombre:nombre,
+    edad:edad,
+    ladrar: function(){
+        console.log("Guauuuu GUAAAAAUU!!!!")
+    }
+}
+
+console.log(perro);
+perro.ladrar();
+
+const dog = {
+    nombre,
+    edad,
+    raza: "callejero",
+    ladrar(){
+        console.log("Guauuuu GUAAAAAUU!!!!")
+    }
+}
+
+console.log(dog);
+dog.ladrar()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
