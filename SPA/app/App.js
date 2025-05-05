5ae1a7453f50bc4bf431899a20b8e941d2ea0fc1
Andres Abdala Alvarez <andrespampa@gmail.com>
2025-05-04 21:23:21 -0300

    Terminando el renderizado dinamico de los posts

--- SPA/app/App.js
+++ SPA/app/App.js
@@ -1,23 +1,37 @@
 import api from './helpers/wp_api.js';
 import {ajax} from './helpers/ajax.js'; //Utilizo la destructuracion porque no utilice la exportacion por default
+import {Title} from './components/Title.js';
+import { Loader } from './components/Loader.js';
+import { Header } from './components/Header.js';
+import { Posts } from './components/Posts.js';
+import { PostCard } from './components/PostCard.js';
 
 
 export function App(){
-    document.getElementById("root").innerHTML =`<h1>Bienvenidos a mi primer SPA con vanilla JS </h1>`;
+    const d = document;
+    const $root = d.getElementById("root");
+    
+    $root.appendChild(Header()); //Aca estoy llamando a la funcion title, que me devuelve un h1 y lo agregamos al root
+    $root.appendChild(Posts());
+    $root.appendChild(Loader());
 
     ajax({
-        url:api.POSTS,
-        cbSuccess: json => {
-            console.log(json);
-        }
-    });
+        url: api.POSTS,
+        cbSuccess: (posts) =>{
+            console.log(posts);
 
-    ajax({
-        url: api.CATEGORIES,
-        cbSuccess: json =>{
-            console.log(json);
+            let html = "";
+            posts.forEach(post => {html += PostCard(post)});
+            d.querySelector(".loader").computedStyleMap.display = "none";
+            d.getElementById("posts").innerHTML = html;
         }
     })
-}
 
-console.log(api);
+    //Prueba de error
+    // ajax({
+    //     url: "tuviejaentanga",
+    //     cbSuccess: (post) =>{
+    //         console.log(post);
+    //     }
+    // })
+}
